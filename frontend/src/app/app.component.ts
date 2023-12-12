f7cb47ab4553fb67251a2eff634a49a2cc96661f
Christian Lasso <lassoc822@gmail.com>
2023-12-12 13:36:08 -0600

    enable

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./components/navbar/navbar.component";
+import { ListProductsComponent } from './components/list-products/list-products.component';
+
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, NavbarComponent]
+    imports: [CommonModule, RouterOutlet, NavbarComponent, ListProductsComponent]
 })
 export class AppComponent {
   title = 'frontend';
--- frontend/src/app/components/list-products/list-products.component.ts
+++ frontend/src/app/components/list-products/list-products.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../../interfaces/product';
 
 @Component({
   selector: 'app-list-products',
@@ -7,6 +8,16 @@ import { Component } from '@angular/core';
   templateUrl: './list-products.component.html',
   styleUrl: './list-products.component.css'
 })
-export class ListProductsComponent {
+export class ListProductsComponent implements OnInit{
+    listProducts:Product [] = [
+    {name: 'Coca Cola', description:'Bebida con azucar', price:4, stock:200},
+    {name: 'Corona', description:'Bebida con alcohol', price:5, stock:300},
+  ]
+
+  constructor() {}
+   
+  ngOnInit(): void {
+    
+  }
 
 }
